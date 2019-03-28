c0d4ec2e452fcc39c0d22c3e98e7e056a1a96dd1
cdaringe <christopher.dieringer@gmail.com>
2019-03-27 21:48:59 -0700

    feat(ssr-react): permit sync api

--- packages/ssr-react/cowtown-ssr-react.ts
+++ packages/ssr-react/cowtown-ssr-react.ts
@@ -3,14 +3,14 @@ import * as ReactDOMServer from 'react-dom/server'
 import Koa from 'koa'
 
 export interface SsrReactOpts {
-  render: <P = {}> (ctx: Koa.Context) => Promise<React.ReactElement<P>>
+  render: <P = {}> (ctx: Koa.Context) => React.ReactElement<P> | Promise<React.ReactElement<P>>
 }
 
 export function createMiddleware (opts: SsrReactOpts) {
   return async function renderDomMiddleware (ctx, next) {
     await next()
     ctx.type = 'html'
-    const component = await opts.render(ctx)
+    const component = await Promise.resolve(opts.render(ctx))
     ctx.body = ReactDOMServer.renderToNodeStream(component)
   } as Koa.Middleware
 }
