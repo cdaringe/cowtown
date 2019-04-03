1a8e4879b5bc505835efb3c6d318ff41b8bdaffc
cdaringe <christopher.dieringer@gmail.com>
2019-04-02 21:48:13 -0700

    chore: increase publish logging

--- packages/docs/scripts/publish.js
+++ packages/docs/scripts/publish.js
@@ -1,12 +1,16 @@
 const ghpages = require('gh-pages')
 const path = require('path')
 
-const buildDirname = path.resolve(__dirname, 'public')
+const buildDirname = path.resolve(__dirname, '..', 'public')
 
+console.log('starting publish')
 ghpages.publish(buildDirname, {
   repo: `https://${process.env.GH_TOKEN}@github.com/cdaringe/cowtown.git`,
   silent: true
 }, err => {
-  if (err) throw err
+  if (err) {
+    console.error(err.message)
+    throw err
+  }
   console.log('docs published')
 })
