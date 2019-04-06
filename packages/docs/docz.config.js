2a1f6dac4b5ae0617f2b9a3c2ad4afe9027653a3
cdaringe <christopher.dieringer@gmail.com>
2019-04-06 00:38:08 -0700

    feat: migrate to docz

---
+++ packages/docs/docz.config.js
@@ -0,0 +1,26 @@
+export default {
+  title: 'cowtown',
+  description: 'a comprehensive guide to server application delivery',
+  // files: '**/*.{mdx}',
+  // typescript: false,
+  // debug: true,
+  menu: [
+    'introduction',
+    'goals',
+    'getting started',
+    'create a middleware',
+    'stack middlewares',
+    'create a basic server',
+    'create hello world',
+    'perform naive routing',
+    'perform basic routing',
+    'manage configuration',
+    'gracefully update configuration',
+    'upload files',
+    'serve static files',
+    'serve an api and a ui',
+    'isomorphic rendering',
+    'logging',
+    'security'
+  ]
+}
